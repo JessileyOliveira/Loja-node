0f4569b90f7e98034fecdeaea773b347713aa4cc
jessiley.oliveira@iv2.com.br <Ch@rmander19>
2018-11-06 14:00:05 -0200

    first
    primeiro commit

---
+++ src/Route/index.js
@@ -0,0 +1,8 @@
+module.exports = function(app){
+    app.get('/', function (req, res, next) {
+        res.status(200).send({
+            title: "Node Express API",
+            version: "0.0.1"
+        });
+    });
+};
---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+var app = express();
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+const consign = require('consign');
+
+const consignProperties = {
+  cwd: "src",
+  locale: 'pt-br',
+  logger: console,
+  verbose: true,
+  extensions: [ '.js', '.json', '.node' ],
+  loggingType: 'info'
+}
+
+consign(consignProperties)
+  .include('Model')
+  .then('Controller')
+  .then("Route")
+  .into(app);
+
+module.exports = app;
