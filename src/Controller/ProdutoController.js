3f07bc786c8a7bde7808adf2d568f2ce8c0405aa
jessiley <jessiley.oliveira@iv2.com.br>
2018-11-08 23:18:18 -0200

    novos verbos para produto
    Inserido o verbo get para pegar por _id, get para pegra por codigo e put para dar update.

--- src/Controller/ProdutoController.js
+++ src/Controller/ProdutoController.js
@@ -3,8 +3,28 @@ module.exports = function(app){
     var controller = {};
     var produto = app.Model.ProdutoModel;
 
-    controller.getProdutos = function(req, res, next){ 
+    controller.getAll = function(req, res, next){ 
         produto.find({}, (err, data) => {
+            if (err) {
+                return  res.status(500).json({ error: true, data: err });
+            }
+            return  res.status(201).json({ error: false, data: data });
+        });
+    }
+
+    controller.getById = function(req, res, next){ 
+        var id = req.params.id;
+        produto.findOne({_id: id}, (err, data) => {
+            if (err) {
+                return  res.status(500).json({ error: true, data: err });
+            }
+            return  res.status(201).json({ error: false, data: data });
+        });
+    }
+
+    controller.getByCodigo = function(req, res, next){ 
+        var codigo = req.params.codigo;
+        produto.findOne({codigo: codigo}, (err, data) => {
             if (err) {
                 return  res.status(500).json({ error: true, data: err });
             }
@@ -15,6 +35,17 @@ module.exports = function(app){
     controller.insert = (req, res, next) => {
         var body = req.body;
         produto.create(body, (err, data) => {
+            if (err) {
+                return  res.status(500).json({ error: true, data: err });
+            }
+            return  res.status(201).json({ error: false, data: data });
+        });
+    };
+
+    controller.update = (req, res, next) => {
+        var body = req.body;
+        var id = req.params._id;
+        produto.update({_id: id}, body, (err, data) => {
             if (err) {
                 return  res.status(500).json({ error: true, data: err });
             }
--- src/Route/ProdutoRoute.js
+++ src/Route/ProdutoRoute.js
@@ -2,7 +2,10 @@ module.exports = function(app){
 
 const controller = app.Controller.ProdutoController;
 
-app.get('/produto', controller.getProdutos);
-app.post('/produto', controller.insert);
+app.get('/produto/getAll', controller.getAll);
+app.get('/produto/getById/:id', controller.getById);
+app.get('/produto/getByCodigo/:codigo', controller.getByCodigo);
+app.post('/produto/insert', controller.insert);
+app.put('/produto/update/:_id', controller.update);
 
 }
